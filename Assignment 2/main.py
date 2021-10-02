1fc117b8c4ccca9caf3075950dcbef7ee808bb32
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-02 17:51:14 -0400

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,4 +1,10 @@
-# Write your assignment here
+import torch
+import numpy as np
 
-if __name__ == '__main__':
-    print('hello')
+#print(torch.cuda.is_available())
+Input = np.loadtxt("even_mnist.csv")
+training_data = Input[3000:29493,:]
+test_data = Input[0:3000,:]
+print(Input.shape)
+print(training_data.shape)
+print(test_data.shape)
