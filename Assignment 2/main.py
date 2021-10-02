ca0be0a1ca9b260f6f2d290ebc9729ffe876b3d1
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-02 18:49:18 -0400

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,10 +1,18 @@
 import torch
+import matplotlib.pyplot as plt
 import numpy as np
 
 #print(torch.cuda.is_available())
 Input = np.loadtxt("even_mnist.csv")
-training_data = Input[3000:29493,:]
-test_data = Input[0:3000,:]
-print(Input.shape)
-print(training_data.shape)
-print(test_data.shape)
+
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 5, 3
+for n in range(15):
+    sample = Input[n, 0:197]
+    picture = np.zeros([14, 14], int)
+    for i in range(196):
+        picture[i // 14, i % 14] = sample[i]
+    figure.add_subplot(rows, cols, n+1)
+    plt.axis("off")
+    plt.imshow(np.array(picture), cmap="gray")
+plt.show()
