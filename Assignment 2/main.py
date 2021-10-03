44fba162f37ee9d92f891ff85d9abf572f39f6a2
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-02 22:15:42 -0400

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -4,6 +4,7 @@
 
 import torch as T
 import numpy as np
+from torch import nn
 
 #print(torch.cuda.is_available())
 #Input = np.loadtxt("even_mnist.csv")
@@ -15,7 +16,7 @@ device = T.device("cuda" if T.cuda.is_available() else "cpu")
 class DigitsDataset(T.utils.data.Dataset):
 
   def __init__(self, src_file, TEST):
-    if TEST == True:
+    if TEST == True:            #use random split?
       x_tmp = np.loadtxt(src_file, max_rows=3000, usecols=range(0, 196))
       y_tmp = np.loadtxt(src_file, max_rows=3000, usecols=196)
     else:
@@ -39,7 +40,29 @@ class DigitsDataset(T.utils.data.Dataset):
 
 train_dataset = DigitsDataset("even_mnist.csv", TEST=False)
 test_dataset = DigitsDataset("even_mnist.csv", TEST=True)
-train_dataloader = T.utils.data.DataLoader(train_dataset, batch_size=26492, shuffle=True)
 
-print(train_dataset.__len__())
-print(test_dataset.__len__())
+train_dataloader = T.utils.data.DataLoader(train_dataset, batch_size=26492, shuffle=True)
+test_dataloader = T.utils.data.DataLoader(test_dataset, batch_size=3000, shuffle=True)
+
+class NeuralNetwork(nn.Module):
+  def __init__(self):
+    super(NeuralNetwork, self).__init__()
+    self.linear_relu_stack = nn.Sequential(
+      nn.Linear(14 * 14, 130),
+      nn.ReLU(),
+      nn.Linear(130, 130),
+      nn.ReLU(),
+      nn.Linear(130, 5),
+    )
+
+  def forward(self, x):
+    logits = self.linear_relu_stack(x)
+    return logits
+
+model = NeuralNetwork().to(device)
+
+X = T.rand(1, 196, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
