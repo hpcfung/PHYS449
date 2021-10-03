2c6b09ab0ee38612225bfb20ba12992a3ce57360
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-02 21:30:49 -0400

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -14,11 +14,13 @@ device = T.device("cuda" if T.cuda.is_available() else "cpu")
 
 class DigitsDataset(T.utils.data.Dataset):
 
-  def __init__(self, src_file = "even_mnist.csv", num_rows=26492):
-    x_tmp = np.loadtxt(src_file, max_rows=num_rows,
-      usecols=range(0,196))
-    y_tmp = np.loadtxt(src_file, max_rows=26492,
-      usecols=196)
+  def __init__(self, src_file, TEST):
+    if TEST == True:
+      x_tmp = np.loadtxt(src_file, max_rows=3000, usecols=range(0, 196))
+      y_tmp = np.loadtxt(src_file, max_rows=3000, usecols=196)
+    else:
+      x_tmp = np.loadtxt(src_file, skiprows=3000, usecols=range(0, 196))
+      y_tmp = np.loadtxt(src_file, skiprows=3000, usecols=196)
 
     self.x_data = T.tensor(x_tmp).to(device)
     self.y_data = T.tensor(y_tmp).to(device)
@@ -34,3 +36,10 @@ class DigitsDataset(T.utils.data.Dataset):
     sample = \
       { 'image' : img, 'digit' : dgt }
     return sample
+
+train_dataset = DigitsDataset("even_mnist.csv", TEST=False)
+test_dataset = DigitsDataset("even_mnist.csv", TEST=True)
+train_dataloader = T.utils.data.DataLoader(train_dataset, batch_size=26492, shuffle=True)
+
+print(train_dataset.__len__())
+print(test_dataset.__len__())
