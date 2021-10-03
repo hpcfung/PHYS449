34de8edee139a35b4d9bfd23624915d64acd2deb
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-02 23:05:51 -0400

    Update main.py

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -11,7 +11,13 @@ from torch import nn
 #training_data = Input[3000:29493,:]
 #test_data = Input[0:3000,:]
 
-device = T.device("cuda" if T.cuda.is_available() else "cpu")
+#device = T.device("cuda" if T.cuda.is_available() else "cpu")
+device = T.device("cpu")
+
+hidden_layer_width = 200
+learning_rate = 1e-3
+batch_size = 26492
+epochs = 20
 
 class DigitsDataset(T.utils.data.Dataset):
 
@@ -30,13 +36,13 @@ class DigitsDataset(T.utils.data.Dataset):
     return len(self.x_data)  # required
 
   def __getitem__(self, idx):
-    if T.is_tensor(idx):
-      idx = idx.tolist()
+    #if T.is_tensor(idx):
+    #  idx = idx.tolist()
     img = self.x_data[idx, 0:196]
-    dgt = self.y_data[idx]
-    sample = \
-      { 'image' : img, 'digit' : dgt }
-    return sample
+    dgt = T.div(self.y_data[idx], 2, rounding_mode='floor')
+    #sample = \
+    #  { 'image' : img, 'digit' : dgt }
+    return img.float(), dgt.long()
 
 train_dataset = DigitsDataset("even_mnist.csv", TEST=False)
 test_dataset = DigitsDataset("even_mnist.csv", TEST=True)
@@ -48,11 +54,11 @@ class NeuralNetwork(nn.Module):
   def __init__(self):
     super(NeuralNetwork, self).__init__()
     self.linear_relu_stack = nn.Sequential(
-      nn.Linear(14 * 14, 130),
+      nn.Linear(14 * 14, hidden_layer_width),
       nn.ReLU(),
-      nn.Linear(130, 130),
+      nn.Linear(hidden_layer_width, hidden_layer_width),
       nn.ReLU(),
-      nn.Linear(130, 5),
+      nn.Linear(hidden_layer_width, 5),
     )
 
   def forward(self, x):
@@ -61,8 +67,53 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 
-X = T.rand(1, 196, device=device)
-logits = model(X)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
+#X = T.rand(1, 196, device=device)
+#logits = model(X)
+#pred_probab = nn.Softmax(dim=1)(logits)
+#y_pred = pred_probab.argmax(1)
+#print(f"Predicted class: {y_pred}")
+
+
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+  size = len(dataloader.dataset)
+  for batch, (X, y) in enumerate(dataloader):
+    # Compute prediction and loss
+    pred = model(X)
+    loss = loss_fn(pred, y)
+
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if batch % 100 == 0:
+      loss, current = loss.item(), batch * len(X)
+      print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+  size = len(dataloader.dataset)
+  num_batches = len(dataloader)
+  test_loss, correct = 0, 0
+
+  with T.no_grad():
+    for X, y in dataloader:
+      pred = model(X)
+      test_loss += loss_fn(pred, y).item()
+      correct += (pred.argmax(1) == y).type(T.float).sum().item()
+
+  test_loss /= num_batches
+  correct /= size
+  print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = T.optim.SGD(model.parameters(), lr=learning_rate)
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
