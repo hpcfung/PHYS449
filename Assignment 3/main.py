e47ef996d74b477166683d5a59528e82f65df346
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-10-30 15:46:40 -0400

    Update main.py
    Implemented neural network, plot solutions.

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,28 +1,43 @@
 import numpy as np
 from pylab import plot, show
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.backends.backend_pdf import PdfPages #remove later
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 if __name__ == '__main__':
+    #np.random.seed(0)
+
     initial_x = 0.5
     initial_y = 0.6
-    T = 10
-    N = 1000
+    T = 7
+    solution_steps = 500
+    step_size_in_training_data = 25
+    training_sol_steps = 20 #when mmultiplied, these 2 should give sol steps
 
     IC_lower_bound = -1
     IC_upper_bound = 1
 
+    IC_factor = 2
     scale_factor = 3
 
     lower_bound = IC_lower_bound*scale_factor
     upper_bound = IC_upper_bound*scale_factor
-    plot_grid_size = 30
+    plot_grid_size = 20
+    IC_grid_size = 8
 
-    num_DE = 5
+    num_sol = IC_grid_size**2
 
-    time_step = T / N
+    num_DE = 4
 
-    pp = PdfPages('numerical_DEs.pdf')
+    time_step = T / solution_steps
+
+    pp = PdfPages('MLP_DE_solver.pdf')
+
+    sol_set = np.zeros((num_sol,training_sol_steps,2), dtype=np.float32)
+    IC_set = np.zeros((num_sol,2), dtype=np.float32)
 
     def u0(x, y):
         return -y / np.sqrt(x ** 2 + y ** 2)
@@ -42,7 +57,7 @@ if __name__ == '__main__':
         return 1/(y**4+5)
 
     def v2(x, y):
-        return np.sin(3*y)/2
+        return np.sin(4*y)/2+0.05
 
 
     def u3(x, y):
@@ -59,14 +74,14 @@ if __name__ == '__main__':
         return y
 
     def v4(x, y):
-        return -x-x**3-2*y
-
+        return -x-x**3-0.5*y
 
     def u5(x, y):
-        return 2*x-2*x*y
+        return np.sin(np.pi*x) + np.sin(np.pi*y)
 
     def v5(x, y):
-        return -y+x*y
+        return np.cos(np.pi*y)
+
 
     def plot_vector_field(u, v):
         X, Y = np.meshgrid(np.linspace(lower_bound, upper_bound, plot_grid_size),
@@ -79,14 +94,6 @@ if __name__ == '__main__':
                 V[i, j] = v(X[i, j], Y[i, j])
 
         plt.quiver(X, Y, U, V)
-
-    def rk2(x0, y0, Delta_t, u, v):
-        k1x = u(x0, y0) * Delta_t
-        k1y = v(x0, y0) * Delta_t
-        k2x = u(x0 + k1x / 2, y0 + k1y / 2) * Delta_t
-        k2y = v(x0 + k1x / 2, y0 + k1y / 2) * Delta_t
-        return x0 + k2x, y0 + k2y
-
 
     def rk4(x0, y0, Delta_t, u, v):
         k1x = u(x0, y0) * Delta_t
@@ -102,60 +109,146 @@ if __name__ == '__main__':
     # no x dep, so that term in the rk method should not matter?
     # just imagine setting output var from 1 to 2?
 
-    def solve_DE(u, v, index):
-        x_points_rk2 = [initial_x]
-        y_points_rk2 = [initial_y]
-        x_points_rk4 = [initial_x]
-        y_points_rk4 = [initial_y]
-        t_points = [0]
+    def solve_DE(u, v, init_x, init_y, index):
+        x_points_rk4 = [init_x]
+        y_points_rk4 = [init_y]
 
-        t = 0
-        for k in range(N):
-            t = t + time_step
-
-            last_x_rk2 = x_points_rk2[k]
-            last_y_rk2 = y_points_rk2[k]
-            new_x_rk2, new_y_rk2 = rk2(last_x_rk2, last_y_rk2, time_step, u, v)
-            x_points_rk2.append(new_x_rk2)
-            y_points_rk2.append(new_y_rk2)
-
+        tmp = 0
+        for k in range(solution_steps+1):
             last_x_rk4 = x_points_rk4[k]
             last_y_rk4 = y_points_rk4[k]
             new_x_rk4, new_y_rk4 = rk4(last_x_rk4, last_y_rk4, time_step, u, v)
             x_points_rk4.append(new_x_rk4)
             y_points_rk4.append(new_y_rk4)
 
-            t_points.append(t)
+            if k%step_size_in_training_data==0 and k!=0:
+                #print(tmp)
+                sol_set[index, tmp, 0] = np.float32(new_x_rk4)
+                sol_set[index, tmp, 1] = np.float32(new_y_rk4)
+                tmp+=1
 
-            if k % 100 == 0:
-                label = f"t = {t}"
-                plt.annotate(label,  # this is the text
-                             xy=(new_x_rk2, new_y_rk2),  # these are the coordinates to position the label
-                             xytext=(new_x_rk2 - 0.5, new_y_rk2 + 0.5),
-                             arrowprops=dict(arrowstyle="->"))
-                plt.annotate(label,  # this is the text
-                             xy=(new_x_rk4, new_y_rk4),  # these are the coordinates to position the label
-                             xytext=(new_x_rk4 - 0.5, new_y_rk4 + 0.5),
-                             arrowprops=dict(arrowstyle="->"))
-
-        plot_vector_field(u, v)
-        plot(x_points_rk2, y_points_rk2, linestyle='-', color='g', label="rk2")
-        plot(x_points_rk4, y_points_rk4, linestyle='-', color='r', label="rk4")
-        plot(initial_x,initial_y, marker='o', color='b')
-        name = str(index)
-        plt.title("DE "+name)
-        #plt.savefig('plot_of_DE_'+name+'.pdf')
-        plt.legend(loc='lower right')
-        pp.savefig()
-        plt.clf()
+        #plot(x_points_rk4, y_points_rk4, linestyle='--')
+        #plot(init_x,init_y, marker='o', color='b')
         #show()
 
-    u_list = [u1, u2, u3, u4, u5]
-    v_list = [v1, v2, v3, v4, v5]
+    #prepare dataset
+    u_list = [u1, u2, u3, u4, u0, u5]
+    v_list = [v1, v2, v3, v4, v0, v5]
 
-    for I in range(num_DE):
-        u = u_list[I]
-        v = v_list[I]
-        solve_DE(u, v, I+1)
+    I = 4
+    u = u_list[I]
+    v = v_list[I]
+    plot_vector_field(u, v)
+    counter = 0
+    for J in np.linspace(IC_lower_bound*IC_factor, IC_upper_bound*IC_factor, IC_grid_size):
+        for K in np.linspace(IC_lower_bound*IC_factor, IC_upper_bound*IC_factor, IC_grid_size):
+            #print(f"{counter}: ({J},{K})")
+            IC_set[counter, 0] = np.float32(J)
+            IC_set[counter, 1] = np.float32(K)
+            solve_DE(u, v, J, K, counter)
+            counter += 1
 
+    print("training set generated")
+
+    device = torch.device("cpu")
+    training_sol = torch.tensor(sol_set).to(device)
+    training_IC = torch.tensor(IC_set).to(device)
+
+    #training_input = np.zeros((training_sol_steps,1,1), dtype = np.float32)
+    #training_input_tensor = torch.tensor(training_input).to(device)
+
+    N = 2000 #1000
+    learning_rate = 1e-3 #2e-3
+    beta = (0.9,0.99)
+    hidden_layer_width1 = 100
+    hidden_layer_width2 = 100
+    hidden_layer_width3 = 100
+
+    class NeuralNetwork(nn.Module):
+
+        def __init__(self):
+            super(NeuralNetwork, self).__init__()
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(2, hidden_layer_width1),
+                nn.LeakyReLU(),
+                nn.Linear(hidden_layer_width1, hidden_layer_width2),
+                nn.LeakyReLU(),
+                nn.Linear(hidden_layer_width2, hidden_layer_width3),
+                nn.LeakyReLU(),
+                nn.Linear(hidden_layer_width3, training_sol_steps*2),
+            )
+
+        def forward(self, ICs):
+            raw_output = self.linear_relu_stack(ICs)
+            pred_x_list, pred_y_list = torch.split(raw_output, training_sol_steps)
+            return torch.stack([pred_x_list,pred_y_list],dim=-1)
+
+
+    model = NeuralNetwork().to(device)
+    loss_fcn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=beta)
+
+    for epoch in range(N):
+        optimizer.zero_grad()
+
+        loss = 0
+        for J in range(num_sol):
+            output_seq = model(training_IC[J,:])
+            #print(output_seq.size())
+            #S = training_sol[J,:,0,:]
+            #print(f"output_seq size = {output_seq.size()}")
+            #print(f"training_sol size = {training_sol[J,:,:].size()}")
+            loss += loss_fcn(output_seq, training_sol[J,:,:])
+        loss = loss/num_sol
+        loss.backward()
+        optimizer.step()
+        if epoch % 100 == 0 or epoch == (N-1):
+            print(f"epoch = {epoch}   loss = {loss}")
+
+    print("training complete")
+
+
+    def testing(c):
+        test_IC = np.random.uniform(low=IC_lower_bound, high=IC_upper_bound, size=2)
+        test_input = torch.tensor(np.array(test_IC, dtype = np.float32)).to(device)
+        output_seq = model(test_input)
+
+        output_seq_cpu = output_seq.cpu()
+        output_SEQ = output_seq_cpu.detach().numpy()
+
+        output_x = np.insert(output_SEQ[:,0],0,test_IC[0]) #add the IC to the plot
+        output_y = np.insert(output_SEQ[:,1],0,test_IC[1])
+
+        plot(output_x, output_y, linestyle='-', marker='o', markersize=2, color = c)
+        plot(test_IC[0],test_IC[1], marker='o', color = c)
+        #plt.legend(loc='lower right')
+
+    def color_code(num):
+        if num%7==0:
+            return 'b'
+        if num%7==1:
+            return 'g'
+        if num%7==2:
+            return 'r'
+        if num%7==3:
+            return 'c'
+        if num%7==4:
+            return 'm'
+        if num%7==5:
+            return 'y'
+        if num%7==6:
+            return 'k'
+
+    for J in range(7): #7
+        testing(color_code(J))
+
+    name = str(N)
+    plt.title("DE with " + name + " iterations")
+
+    torch.save(model.state_dict(), name+"iterations.pt")
+
+    pp.savefig()
+    show()
     pp.close()
+
+#problem: sol does not start at IC
